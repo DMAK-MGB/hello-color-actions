f2b71c6f5692bb0ce58cfe04464838f1ee87b1ca
DMAK-MGB <dkofman@partners.org>
2024-03-23 14:12:42 -0400

    updated hello messgae

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from the Arrakis - the spice must flow!";
 console.log(hello());
 exports.hello = hello;
